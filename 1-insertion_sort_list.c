33f17419f1d58333f1ca3f62fda7b90501481fa6
Benonking <benonkighoma@gmail.com>
2023-01-20 10:25:36 -0800

    bubble sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,18 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts doubly linked list of integers in ascending order
+ * list: head of linked list
+ * Return: Nothing
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	int i;
+	while (list->next != NULL)
+	{
+		if (list->n < list->next->n)
+	}
+
+}
+
